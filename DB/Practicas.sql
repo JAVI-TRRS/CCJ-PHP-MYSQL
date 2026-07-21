5ecd898d5f64ef4e819acc55d51066ca4b832202
JAVI-TRRS <carlosjaviert327@gmail.com>
2026-07-21 22:13:10 +0000

    se realizaron cambios pequeños en la base de datos

--- DB/Practicas.sql
+++ DB/Practicas.sql
@@ -7,7 +7,7 @@ CREATE TABLE Usuarios (
     apellidos VARCHAR(100) NOT NULL,
     telefono VARCHAR(15) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
-    direccion VARCHAR(200) NOT NULL,
-    correo VARCHAR(100) UNIQUE NOT NULL,
+    direccion VARCHAR(150) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     contrasena VARCHAR(100) NOT NULL    
 );
