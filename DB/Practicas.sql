ebefbef9454321e47857e28794014d20c65998e1
JAVI-TRRS <carlosjaviert327@gmail.com>
2026-07-22 17:41:34 +0000

    se esta dockerizandonel proyecto faltan arreglar unas pequeñas cosas xd

--- DB/Practicas.sql
+++ DB/Practicas.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE Practicas;
-USE DATABASE Practicas;
+CREATE DATABASE IF NOT EXISTS Practicas;
+USE Practicas;
 
-CREATE TABLE Usuarios (
+CREATE TABLE IF NOT EXISTS Usuarios (
     cod_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nombres VARCHAR(100) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
     telefono VARCHAR(15) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
     direccion VARCHAR(150) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    contrasena VARCHAR(100) NOT NULL    
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL
 );
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: PHP/Register_Users.php');
+exit;
